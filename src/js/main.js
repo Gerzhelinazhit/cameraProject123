29de2771b24fa5dad13254edbf2187b630e912ba
Darya Chyruk <darya.chyruk@gmail.com>
2018-10-16 09:33:13 +0300

    try to change videoinput 8

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,12 @@ var counter = 0;
 
 function gotDevices(deviceInfos) {
   // Handles being called several times to update labels. Preserve values.
-  const values = selectors.map(select => select.value);
+  /*const values = selectors.map(select => select.value);
   selectors.forEach(select => {
     while (select.firstChild) {
       select.removeChild(select.firstChild);
     }
-  });
+  });*/
 
   for (var i = 0; i !== deviceInfos.length; ++i) {
     const deviceInfo = deviceInfos[i];
@@ -20,23 +20,18 @@ function gotDevices(deviceInfos) {
     option.value = deviceInfo.deviceId;
     if ((deviceInfo.kind === 'videoinput') && deviceInfo.label.match(/back/i)) {
       console.log(option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`);
-        option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
-        videoSelect.appendChild(option);
+      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+      videoSelect.appendChild(option);
     }
-    // else {
-      // console.log('Some other kind of source/device: ', deviceInfo);
-    //}
   }
-  selectors.forEach((select, selectorIndex) => {
-    //if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
+  selectors[0].value = values[0];
+
+  /*selectors.forEach((select, selectorIndex) => {
       select.value = values[selectorIndex];
-    //}
-  });
+  });*/
 }
 console.log(navigator.mediaDevices.enumerateDevices());
 navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
-
-
 
 
 function gotStream(stream) {
