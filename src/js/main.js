e91e659f90ec59f1a5fd884db51154a22cf2b387
Darya Chyruk <darya.chyruk@gmail.com>
2018-10-16 09:51:15 +0300

    try to change videoinput 11

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@
 const videoElement = document.querySelector('video');
 const videoSelect = document.querySelector('select#videoSource');
 const selectors = [ videoSelect];
+const regex1 = /(back)/g;
+const regex = /(Back)/g;
 var counter = 0;
 
 function gotDevices(deviceInfos) {
@@ -18,19 +20,25 @@ function gotDevices(deviceInfos) {
     const deviceInfo = deviceInfos[i];
     const option = document.createElement('option');
     option.value = deviceInfo.deviceId;
-    if ((deviceInfo.kind === 'videoinput') && deviceInfo.label.match(/back/i)) {
+    if (deviceInfo.kind === 'videoinput' && deviceInfo.label.match('back')) {//&& (regex.test(deviceInfo.label)|| regex1.test(deviceInfo.label))) {
       console.log(option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`);
-      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
-      videoSelect.appendChild(option);
+        option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+        videoSelect.appendChild(option);
     }
+    // else {
+      // console.log('Some other kind of source/device: ', deviceInfo);
+    //}
   }
-
   selectors.forEach((select, selectorIndex) => {
+    if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
       select.value = values[selectorIndex];
+    }
   });
 }
 console.log(navigator.mediaDevices.enumerateDevices());
 navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+
 
 
 function gotStream(stream) {
