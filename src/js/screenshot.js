44cebb8d3dcacb16588664a373fff059e16e136f
Zayats_A <bublegumm98@gmail.com>
2018-10-18 15:09:59 +0300

    screenshot fix

--- src/js/screenshot.js
+++ src/js/screenshot.js
@@ -6,6 +6,7 @@ video = document.querySelector('video');
 canvas = document.createElement('canvas');
 
 screenshotButton.onclick = video.onclick = function() {
+  childCount();
   canvas.width = window.innerWidth;//video.videoWidth;
   canvas.height = window.innerHeight;//video.videoHeight;
   canvas.getContext('2d').drawImage(video, 0, 0);
@@ -26,4 +27,12 @@ screenshotButton.onclick = video.onclick = function() {
 function handleSuccess(stream) {
   screenshotButton.disabled = false;
   video.srcObject = stream;
+}
+
+function childCount(){
+  // let count = document.getElementById('image-container').childElementCount;
+  // return count;
+  if(document.getElementById('image-container').childElementCount > 5){
+    document.getElementById('image-container').removeChild(document.getElementsByTagName('img')[0]);
+  }
 }
