50d9f03a57bbcb71b26759c1d0a84fdc8a517186
Darya Chyruk <darya.chyruk@gmail.com>
2018-10-23 14:32:02 +0300

    hexahedron added to cube

--- src/js/3d_cube.js
+++ src/js/3d_cube.js
@@ -1,37 +1,66 @@
+var hex_height = 3*Math.sqrt(3)/2*1.2;
 init();
 animate();
 
 var camera, scene, renderer, cube;
 
 function init() {
-  camera = new THREE.PerspectiveCamera( 95, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
   camera.position.z = 0;
     console.log("3d camera fov is",camera.fov);
     console.log("3d camera position z", camera.position.z);
-  controls = new THREE.DeviceOrientationControls( camera );
+  //controls = new THREE.DeviceOrientationControls( camera );
   scene = new THREE.Scene();
   container = document.querySelector(".video_container");
 
-// create the shape
- var geometry = new THREE.BoxBufferGeometry(4,4,4);
-  //geometry.scale( - 1, 1, 1 );
- //create a material color or image texture
+ var geometry = new THREE.BoxBufferGeometry( 3, 3, 3 );
  var material = new THREE.MeshBasicMaterial({color: 0x00FFFF, wireframe: true});
  cube = new THREE.Mesh(geometry,material);
  scene.add(cube);
 
-  var geometry = new THREE.CylinderBufferGeometry( 5, 5, 20, 32 );
-  var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-  var cylinder = new THREE.Mesh( geometry, material );
-  scene.add( cylinder );
 
-  //var geometry = new THREE.SphereBufferGeometry( 500, 60, 40 );
-  // invert the geometry on the x-axis so that all of the faces point inward
-  //geometry.scale( - 1, 1, 1 );
- // var material = new THREE.MeshBasicMaterial( { color: 0xff00ff, wireframe: true } );
- // var mesh = new THREE.Mesh( geometry, material );
- // scene.add( mesh );
+  var material2 = new THREE.MeshBasicMaterial( { color: 0x0000ff} );
 
+  hexahedron = new THREE.Geometry();
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,hex_height)); //main plane (then rotation right)
+
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(3,1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(3,-1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,hex_height)); // plane 2
+
+  hexahedron.vertices.push(new THREE.Vector3(3,-1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(3,1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(3,-1.5,0)); //plane 3
+
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,-hex_height)); //plane 4
+
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-3,1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(-3,-1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,-hex_height)); // plane 5
+
+  hexahedron.vertices.push(new THREE.Vector3(-3,-1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(-3,1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-3,-1.5,0));
+
+  line = new THREE.Line( hexahedron, material2 );
+
+  scene.add(line);
 
 
   renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
@@ -40,12 +69,13 @@ function init() {
   renderer.domElement.className = 'd_cube_help';
   container.appendChild( renderer.domElement );
   window.addEventListener( 'resize', onWindowResize, false );
+  window.addEventListener('deviceorientation', setOrientationControls, true);
 }
 
 function animate() {
 
   window.requestAnimationFrame( animate );
-  controls.update();
+  //controls.update();
   renderer.render( scene, camera );
 
 }
@@ -56,6 +86,19 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
 
+}
+
+function setOrientationControls(e) {
+  console.log("mobile orientation test");
+  if (!e.alpha) {
+    return;
+  }
+  controls = new THREE.DeviceOrientationControls(camera, true);
+  controls.connect();
+  controls.update();
+
+  window.removeEventListener('deviceorientation', setOrientationControls, true);
+  console.log("mobile orientation");
 }
 
 
--- src/js/hexahedron.js
+++ src/js/hexahedron.js
@@ -1,66 +1,80 @@
-var hex_height = 3*Math.sqrt(3)/2*1.1;
+var hex_height = 3*Math.sqrt(3)/2*1.2;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 1, 1500 );
-camera.position.set( 0, 0, 0 );
-controls = new THREE.DeviceOrientationControls( camera );
-var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-renderer.setSize( window.innerWidth, window.innerHeight );
-var container = document.querySelector(".video_container");
-container.appendChild( renderer.domElement );
-renderer.domElement.className = "hex_help";
+init();
+animate();
 
-var material = new THREE.MeshBasicMaterial( { color: 0x0000ff} );
+var camera, scene, renderer, line, controls;
 
-hexahedron = new THREE.Geometry();
-hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,hex_height));
-hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,hex_height));
-hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,hex_height));
-hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,hex_height));
-hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,hex_height)); //main plane (then rotation right)
+function init() {
+  camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 1, 1500 );
 
-hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,hex_height));
-hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,hex_height));
-hexahedron.vertices.push(new THREE.Vector3(3,1.5,0));
-hexahedron.vertices.push(new THREE.Vector3(3,-1.5,0));
-hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,hex_height)); // plane 2
+ // controls = new THREE.DeviceOrientationControls(camera);
+  scene = new THREE.Scene();
+  var container = document.querySelector(".video_container");
 
-hexahedron.vertices.push(new THREE.Vector3(3,-1.5,0));
-hexahedron.vertices.push(new THREE.Vector3(3,1.5,0));
-hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,-hex_height));
-hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,-hex_height));
-hexahedron.vertices.push(new THREE.Vector3(3,-1.5,0)); //plane 3
+  var material = new THREE.MeshBasicMaterial( { color: 0x0000ff} );
 
-hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,-hex_height));
-hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,-hex_height));
-hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,-hex_height));
-hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,-hex_height));
-hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,-hex_height)); //plane 4
+  hexahedron = new THREE.Geometry();
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,hex_height)); //main plane (then rotation right)
 
-hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,-hex_height));
-hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,-hex_height));
-hexahedron.vertices.push(new THREE.Vector3(-3,1.5,0));
-hexahedron.vertices.push(new THREE.Vector3(-3,-1.5,0));
-hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,-hex_height)); // plane 5
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(3,1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(3,-1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,hex_height)); // plane 2
 
-hexahedron.vertices.push(new THREE.Vector3(-3,-1.5,0));
-hexahedron.vertices.push(new THREE.Vector3(-3,1.5,0));
-hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,hex_height));
-hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,hex_height));
-hexahedron.vertices.push(new THREE.Vector3(-3,-1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(3,-1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(3,1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(3,-1.5,0)); //plane 3
 
-var line = new THREE.Line ( hexahedron, material );
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(1.5,-1.5,-hex_height)); //plane 4
 
-scene.add(line);
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,-hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-3,1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(-3,-1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,-hex_height)); // plane 5
 
-//camera.position.z = 10;
+  hexahedron.vertices.push(new THREE.Vector3(-3,-1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(-3,1.5,0));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-1.5,-1.5,hex_height));
+  hexahedron.vertices.push(new THREE.Vector3(-3,-1.5,0));
 
-var animate = function () {
+  line = new THREE.Line ( hexahedron, material );
+
+  scene.add(line);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.domElement.className = "hex_help";
+  container.appendChild( renderer.domElement );
+  window.addEventListener( 'resize', onWindowResize, false );
+}
+
+
+function animate() {
   requestAnimationFrame( animate );
   //controls.update();
-
-  //line.rotation.x += 0.005;
   renderer.render( scene, camera );
-};
+}
 
-animate();
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
