891c2fb9cfdee6d42bcb31140344b765a18707a4
Darya Chyruk <darya.chyruk@gmail.com>
2018-10-31 12:49:45 +0300

    buttoms changed

--- src/js/hyroscope_views.js
+++ src/js/hyroscope_views.js
@@ -1,11 +1,17 @@
 var horizontal_hyroscope = document.querySelector('.horizontal_hyroscope');
 var vertical_hyroscope = document.querySelector('.vertical_hyroscope');
 
-var canvas = document.createElement('canvas');
-var context = canvas.getContext("2d");
-var image = new Image();
-image.src = "http://localhost/tile.png";
-image.onload = function(){
-  context.drawImage(image, canvas.width, canvas.height);
-};
-horizontal_hyroscope.appendChild(canvas);
+
+window.addEventListener("resize", function(){
+  vertical_hyroscope.height = 0.9*window.innerHeight;
+  horizontal_hyroscope.width = 0.9*window.innerWidth;
+}, true);
+
+// var canvas = document.createElement('canvas');
+// var context = canvas.getContext("2d");
+// var image = new Image();
+// image.src = "http://localhost/tile.png";
+// image.onload = function(){
+//   context.drawImage(image, canvas.width, canvas.height);
+// };
+// horizontal_hyroscope.appendChild(canvas);
