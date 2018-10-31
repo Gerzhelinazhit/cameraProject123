fa9134e80694ae5c86037bf53e3b5eaa277854e0
Darya Chyruk <darya.chyruk@gmail.com>
2018-10-31 13:25:24 +0300

    double click screen

--- src/js/screenshot.js
+++ src/js/screenshot.js
@@ -5,24 +5,63 @@ video = document.querySelector('video');
 
 canvas = document.createElement('canvas');
 
-screenshotButton.onclick = video.onclick = function() {
-  childCount();
-  canvas.width = window.innerWidth;//video.videoWidth;
-  canvas.height = window.innerHeight;//video.videoHeight;
-  canvas.getContext('2d').drawImage(video, 0, 0);
-  canvas.id = 'screenshot_img';
- // img = document.createElement('img');
+var timeout;
+var lastTap = 0;
 
-  // Other browsers will fall back to image/png
-  let div = document.getElementById('image-container');
-  let img = document.createElement("img");
-  img.className = 'image';
-  div.appendChild(img);
+$('element').on('touchend click',function(e){
+  var currentTime = new Date().getTime();
+  var tapLength = currentTime - lastTap;
 
-  img.src = canvas.toDataURL('src/img');
+  e.preventDefault();
+  clearTimeout(timeout);
 
+  if(tapLength < 500 && tapLength > 0){
 
-};
+    //Double Tap/Click
+    childCount();
+    canvas.width = window.innerWidth;//video.videoWidth;
+    canvas.height = window.innerHeight;//video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    canvas.id = 'screenshot_img';
+    // img = document.createElement('img');
+
+    // Other browsers will fall back to image/png
+    let div = document.getElementById('image-container');
+    let img = document.createElement("img");
+    img.className = 'image';
+    div.appendChild(img);
+
+    img.src = canvas.toDataURL('src/img');
+
+  }else{
+
+    //Single Tap/Click
+
+    timeout = setTimeout(function(){
+      //Single Tap/Click code here
+
+      clearTimeout(timeout);
+    }, 500);
+  }
+  lastTap = currentTime;
+});
+
+// screenshotButton.onclick = video.onclick = function() {
+//   childCount();
+//   canvas.width = window.innerWidth;//video.videoWidth;
+//   canvas.height = window.innerHeight;//video.videoHeight;
+//   canvas.getContext('2d').drawImage(video, 0, 0);
+//   canvas.id = 'screenshot_img';
+//  // img = document.createElement('img');
+//
+//   // Other browsers will fall back to image/png
+//   let div = document.getElementById('image-container');
+//   let img = document.createElement("img");
+//   img.className = 'image';
+//   div.appendChild(img);
+//
+//   img.src = canvas.toDataURL('src/img');
+// };
 
 function handleSuccess(stream) {
   screenshotButton.disabled = false;
