f4521363b8f2946149096f19a64ee656de329712
Darya Chyruk <darya.chyruk@gmail.com>
2018-11-02 16:08:17 +0300

    added universal polygon6

--- src/js/polygon.js
+++ src/js/polygon.js
@@ -4,12 +4,21 @@ animate();
 
 var camera, scene, renderer, figure;
 //var n = input.getTexParameter(); // get text from input, n == amount of planes
-var n = 8;
-var rotation_angle = 360/n;
-var max_margin = 4; // think about it!!!
-var i = 0;
+
+function toDegrees (angle) {
+  return angle * (180 / Math.PI);
+}
+
+function toRadians (angle) {
+  return angle * (Math.PI / 180);
+}
 
 function init(){
+
+  var n = 8;
+  var rotation_angle = 360/n;
+  var max_margin = 4; // think about it!!!
+  var i = 0;
   camera = new THREE.PerspectiveCamera( 150, window.innerWidth / window.innerHeight, 3, 1000 );
   console.log("3d camera fov is",camera.fov);
   console.log("3d camera position z", camera.position.z);
@@ -25,25 +34,29 @@ function init(){
   function writeFigure(rotation_angle, max_margin) {
     figure = new THREE.Geometry();
     var bett = rotation_angle/2;
+    console.log("bett=",bett,", angle=",rotation_angle,", margin=",max_margin);
+
     var x1,y1,y2,z1;
-    y1 = max_margin*Math.tan(bett);
+    y1 = max_margin*Math.sin(toRadians(bett));
     y2 = y1*(-1);
-    z1 = max_margin*Math.cos(bett);
-    x1 = max_margin*Math.sin(bett);
+    z1 = max_margin*Math.cos(toRadians(bett));
+    x1 = max_margin*Math.sin(toRadians(bett));
+    console.log("x1: ",x1," ,y1",y1," ,y2",y2,", z1: ",z1);
 
 
     while (i < n){
       z1 = writePlane(x1,y1,y2,z1,bett,i);
+      console.log("z1=",z1);
       x1 = Math.sqrt(max_margin**2 - z1**2);
       i = i+1;
-    }
+   }
   }
 
   function writePlane(x,y,y2,z,bett,i){
     figure.vertices.push(new THREE.Vector3(x,y,z));
     figure.vertices.push(new THREE.Vector3(x,y2,z));
 
-    var z2 = z*Math.cos((i+1)*rotation_angle+bett)/Math.cos(i*rotation_angle+bett);
+    var z2 = z*Math.cos(toRadians((i+1)*rotation_angle+bett))/Math.cos(toRadians(i*rotation_angle+bett));
     var x2 = Math.sqrt(max_margin**2 - z2**2);
 
     figure.vertices.push(new THREE.Vector3(x2,y,z2));
@@ -51,6 +64,7 @@ function init(){
     line = new THREE.Line( figure, material2 );
 
     scene.add(line);
+    console.log(z2);
     return z2;
   }
 
@@ -61,8 +75,6 @@ function init(){
   container.appendChild( renderer.domElement );
   window.addEventListener( 'resize', onWindowResize, false );
 }
-
-
 
 function animate() {
 
