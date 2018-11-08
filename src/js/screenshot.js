4c0488af8553fa1a61015126bd902b838e024dd8
Darya Chyruk <darya.chyruk@gmail.com>
2018-11-08 14:53:24 +0300

    changed img6

--- src/js/screenshot.js
+++ src/js/screenshot.js
@@ -9,11 +9,11 @@ screenshotButton.textContent = "Take screenshot";
 
 screenshotButton.onclick = cubeClick.onclick = function() {
   var counter = childCount();
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerWidth;
+  canvas.width = 0.9*window.innerWidth;
+  canvas.height = 0.9*window.innerWidth;
   //  resize_canvas.getContext('2d').drawImage(orig_src, 0, 0, width, height);
-  var y_offset = (window.innerHeight-0.9*window.innerWidth)/2;
-  var x_offset = 0.2*window.innerWidth;
+  var y_offset = 0.05*window.window.innerWidth;
+  var x_offset = 0.05*window.window.innerWidth;
   console.log("wid: ",window.innerWidth,", 09H: ",0.9*window.innerHeight,", x offset: ",x_offset,", y offset: ",y_offset);
   canvas.getContext('2d').drawImage(video, x_offset, y_offset, 0.9*window.innerWidth,0.9*window.innerWidth);
   canvas.id = 'screenshot_img';
